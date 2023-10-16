4f818ddb307633cabfeda8c3fa7b9bd81b54e5e5
YousefAis <112547277+YousefAis@users.noreply.github.com>
2023-10-16 07:31:02 +0300

    Update test2.sh

--- test2.sh
+++ test2.sh
@@ -6,11 +6,13 @@ sudo mkdir /opt/odoo
 sudo chown root:odoo /opt/odoo
 
 echo -e "\n---- Clone odoo 11 ----"
-git clone -b 11.0 git@github.com:odoo/odoo.git /opt/odoo/odoo11
+git clone -b 11.0 https://github.com/odoo/odoo.git /opt/odoo/odoo11
+echo -e "\n---- 1 ----"
 sudo chown -hR root:odoo /opt/odoo/odoo11
+echo -e "\n----2 ----"
 sudo apt update; sudo apt dist-upgrade
 
-echo -e "\n---- python 3.5 ----"
+echo -e "\n---- python 3.6 ----"
 
 sudo apt -y install make build-essential libreadline-dev \
 wget curl llvm libssl-dev zlib1g-dev libbz2-dev  \
@@ -18,7 +20,9 @@ libsqlite3-dev libncurses5-dev libncursesw5-dev \
 xz-utils tk-dev libffi-dev liblzma-dev libgdbm-dev \
 libnss3-dev libedit-dev libc6-dev
 
+
 wget https://www.python.org/ftp/python/3.6.15/Python-3.6.15.tgz
+echo -e "\n---- Clone odoo 11 ----"
 
 tar -xzf Python-3.6.15.tgz
 cd Python-3.6.15
@@ -32,13 +36,13 @@ python3.6 -V
 
 echo -e "\n---- python3-pip ----"
 sudo apt -y install python3-pip
-
+echo -e "\n---- 3 ----"
 sudo mkdir -p /python-venv/3.6/odoo11
 
 sudo chown -hR odoo11: /python-venv/3.6/odoo11
-
+echo -e "\n---- 4 ----"
 sudo su - odoo11 -s /bin/bash
-
+echo -e "\n---- 5 ----"
 python3.6 -m venv /python-venv/3.6/odoo11
 
 echo -e "\n----  python dep ----"
@@ -59,19 +63,20 @@ echo -e "\n----less ----"
 sudo npm -y install -g less@3.10.3
 sudo npm -y install -g less-plugin-clean-css
 
+echo -e "\n---- 6 ----"
 wget https://github.com/wkhtmltopdf/packaging/releases/download/0.12.6.1-2/wkhtmltox_0.12.6.1-2.jammy_amd64.deb
-
+echo -e "\n---- 7 ----"
 sudo dpkg -i wkhtmltox_0.12.6.1-2.jammy_amd64.deb
 # force install dependencies
 sudo apt -f install
-
+echo -e "\n---- 8 ----"
 # switch to odoo11's bash
 sudo su - odoo11 -s /bin/bash
 # activate the virtual environment
 source /python-venv/3.6/odoo11
-
+echo -e "\n---- 9 ----"
 pip install --upgrade pip
-
+echo -e "\n---- 10 ----"
 pip install -r /opt/odoo/odoo11/requirements.txt
 
 /opt/odoo/odoo11/odoo-bin
